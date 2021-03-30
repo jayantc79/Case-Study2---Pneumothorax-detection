4f05e4c6375f9db430b667ea9f39ef5e3bd68540
Jayant Chaudhari <61195551+jayantc79@users.noreply.github.com>
2021-03-30 18:41:07 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,24 +1,10 @@
 import numpy as np
 import streamlit as st
 from PIL import Image
-import tensorflow as tf
-from tensorflow.keras import backend as K
-from tensorflow.keras.losses import binary_crossentropy
-import random, re, math
-import tensorflow as tf, tensorflow.keras.backend as K
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras import optimizers
-from tensorflow.keras.models import Sequential
-import tensorflow.keras.layers as L
-from tensorflow.keras.applications import ResNet152V2, InceptionResNetV2, InceptionV3, Xception, VGG19
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Dense, Input, Dropout, Lambda, Conv2D, Conv2DTranspose, MaxPooling2D, Concatenate, Activation, Add, multiply, add, concatenate, LeakyReLU, ZeroPadding2D, UpSampling2D, BatchNormalization, Flatten
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model, load_model
+
+
+
 seg_model = "unet_with_densenet_weights-23-0.4608.hdf5"
 IMG_WIDTH = IMG_HEIGHT = 256
 N_CHANNELS=3
