9be8a9f42070fbd223766939f436db0b6fcc72b9
Jayant Chaudhari <61195551+jayantc79@users.noreply.github.com>
2021-03-30 16:31:34 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import app1
-import app2
-from multiapp import MultiApp
-import streamlit as st
-
-app = MultiApp()
-app.add_app("Introduction", app1.app)
-app.add_app("Get Prediction", app2.app)
-app.run()
